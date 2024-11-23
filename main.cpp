d6c2f81909e9cbc54a874a3d977bc140e73eb144
Hieu <hieunguyen.0527@gmail.com>
2024-11-23 12:36:02 -0500

    set up c++ template

--- main.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by kevin on 11/23/2024.
-//
-
-int main() { std::cout << "THIS IS HIEU" << std::endl; }
---
+++ src/main.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+using namespace std;
+
+int main() { std::cout << "Project 3" << std::endl; }
