6f3fea8f32d81d448f4d3d85a64b283641165a6c
Gabriel Thrasher <148641861+GabrielThrasher@users.noreply.github.com>
2024-11-28 12:25:37 -0600

    Update puzzle_piece.h

--- patrix/src/puzzle_piece.h
+++ patrix/src/puzzle_piece.h
@@ -8,28 +8,33 @@ using namespace std;
 
 class PuzzlePiece {
   public:
-    vector<vector<tuple<int, int, int>>> colors;
-
     int col;
     int row;
 
-    // edges
+    // Edges
     int top;
-    int bottom;
     int left;
+    int bottom;
     int right;
 
+    // Colors
+    vector<vector<tuple<int, int, int>>> colors;
+
+    PuzzlePiece();
     PuzzlePiece(int row, int col, int pieceSize);
 
     void initColor(int pixelResPerPiece);
     bool operator==(const PuzzlePiece &other) const;
+
+    void write(std::ofstream& out) const;
+    int read(const std::vector<char>& buffer, int offset);
 };
 
 // Hash function for (unordered) sets and (unordered) maps' keys
 namespace std {
-template <> struct hash<PuzzlePiece> {
-    size_t operator()(const PuzzlePiece &piece) const noexcept {
-        return hash<int>()(piece.row) ^ hash<int>()(piece.col);
-    }
-};
-} // namespace std
+    template <> struct hash<PuzzlePiece> {
+        size_t operator()(const PuzzlePiece &piece) const noexcept {
+            return hash<int>()(piece.row) ^ hash<int>()(piece.col);
+        }
+    };
+}
